fdd71136e5e38ddce2ccd9f457541d248854aaa1
iamEi <79460406+iamEi@users.noreply.github.com>
2021-09-30 17:59:47 +0800

    revised

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
     document.getElementById("navbar").style.top = "-80px";
+  } else {
+    document.getElementById("navbar").style.top = "0px";
   }
   prevScrollpos = currentScrollPos;
 }
