095cb7d9fa9f7874f3406f814f967a794347279b
iamEi <79460406+iamEi@users.noreply.github.com>
2021-10-05 21:06:55 +0800

    added feature
    inactivity hides nav bar.
    scroll bar change color
    hovering cursor at top of page shows nav bar

--- js/main.js
+++ js/main.js
@@ -15,3 +15,29 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+onmousemove = function(e){
+  if (e.clientY <= 10){
+    this.document.getElementById("navbar").style.top = "0px";
+  }
+  // else if(e.clientY > 200){
+    // this.document.getElementById("navbar").style.top = "-80px";
+  // }
+}
+
+var inactivityTime = function () {
+  var time;
+  document.onscroll = resetTimer;
+
+  function hideNav() {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+
+  function resetTimer() {
+      clearTimeout(time);
+      time = setTimeout(hideNav, 3000)
+  }
+};
+
+
+inactivityTime();
